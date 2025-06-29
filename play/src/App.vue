b9afd05bc09de0ad0f1e63cd0cfc05372491965e
coderhsy <coderhsy>
2025-06-29 17:57:41 +0800

    给buttonn优化默默认颜色

--- play/src/App.vue
+++ play/src/App.vue
@@ -10,7 +10,7 @@ import HButton from '@hsy/components/botton'
       <Alarm />
     </h-icon> -->
     <div class="box">
-      <h-text type="primary">
+      <h-text>
         The -webkit-line-clamp CSS property allows limiting of the contents of a
         block to the specified number of lines.</h-text
       >
@@ -18,14 +18,18 @@ import HButton from '@hsy/components/botton'
         The -webkit-line-clamp CSS property allows limiting of the contents of a
         block to the specified number of lines.</el-text
       >
-      <h-button type="primary">Hello world</h-button>
-      <el-button type="primary">Hello world</el-button>
+      <h-button type="primary" style="margin-bottom: 30px"
+        >Hello world</h-button
+      >
+      <el-button>Hello world</el-button>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .box {
+  /* display: flex; */
+  margin: 100px auto;
   width: 200px;
 }
 </style>
